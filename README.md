08bb6032b3265404c7dd5a27cb6ca704570c9e80
Alexsander-Vilar <alexsandervilar@gmail.com>
2023-05-06 09:18:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Desafio DIO.
+
+##### Este código realiza o calculo da media de 6 números positivos, ignorando os valores negativos.
---
+++ src/MediaDeValoresPositivos.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MediaDeValoresPositivos {
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int cont = 0;
+        double media = 0;
+        double valor = 0;
+        double num = 0;
+
+        System.out.println("Digite seis valores\n");
+
+        for (int i = 0; i < 6; i++) {
+
+            System.out.print("Digite um valor: ");
+            num = leitor.nextDouble();
+
+            if (num > 0) {
+                cont++;
+                valor = valor + num;
+            }
+        }
+        media = valor / cont;
+        System.out.println("\n" + cont + " valores positivos\n");
+        System.out.println(String.format("%.1f", media));
+    }
+}
